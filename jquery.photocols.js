20f3fcc98e3ad9c13bb39acfb850981bb88375ab
Dailos Aday Medina Méndez <dailos@2coders.com>
2014-06-17 13:48:55 +0100

    Animation with setTimeOut instead jQuery Animate

--- jquery.photocols.js
+++ jquery.photocols.js
@@ -14,34 +14,37 @@
 		// set default values
 		// typeNumber < 1 for automatic calculation
 		options	= $.extend( {}, {
-      bgcolor   : '#000',
-      width		 : 'auto',
-      colswidth : '200',
-			height		: 600,
-      gap       : 5,
-      opacity   : 0.3,
-      data      : {}
+      bgcolor    : '#000',
+      width		  : 'auto',
+			colswidth  : 200,
+			itemheight : 300,
+			height		 : 600,
+      gap        : 5,
+      opacity    : 0.3,
+      data       : {}
 		}, options);
 
+		var rows;
 
-    var animate = function ( item ) {
-      $( item ).animate({
-        top: parseInt($(item).position().top + (options.height/2+options.gap)*4)
-      }, {
-        duration: 80000,
-        easing: "linear",
-        complete: function() {
-          animate( this );
-        },
-        step: function( now, fx ) {
-          if ( now  > options.height ) {
-							$(fx.elem).stop();
-              $(fx.elem).css('top', parseInt($(fx.elem).position().top - (options.height/2+options.gap)*4)) ;
-							animate( fx.elem );
 
-          }
-        }
-      });
+    var animate = function (  ) {
+
+			$('.pc-item').each( function (index) {
+
+				if ( $(this).position().top  > options.height ) {
+						$(this).css('top',  $(this).position().top - (options.itemheight+options.gap)*rows) ;
+
+				} else {
+					$(this).css('top', $(this).position().top + 1) ;
+				}
+
+			});
+
+
+			setTimeout(function() {
+    			animate();
+			},50);
+
     };
 
     var resize = function () {
@@ -53,26 +56,28 @@
 			$.fx.off = false;
 
       var cols = Math.floor( element.width() / ( options.colswidth ) );
-      var gapX = Math.floor(element.width() / cols) - options.colswidth ;
+      var width = Math.floor(element.width() / cols) - options.gap ;
 
-      var count= 0;
+			rows = Math.ceil(options.height/options.itemheight)+1;
+
+      var count = 0;
 
       for ( var i= 0 ; i < cols ; i++) {
 
-        var leftposition = element.width() / cols ;
-        var topposition = parseInt(Math.random()*options.height);
+        var leftposition = Math.floor(element.width() / cols) ;
+        var topposition = Math.floor(Math.random() * options.height);
 
-        var col = $('<div id="pc-col'+i+'" class="pc-col" style="position:absolute" />');
+        var col = $('<div id="pc-col'+i+'" class="pc-col" style="position:absolute"  />');
 
         element.append(col);
 
-        for ( var j= 0 ; j < 4 ; j++) {
+        for ( var j= 0 ; j < rows ; j++) {
 
           var item = options.data[count++ % options.data.length];
 
               col.append( '<a class="pc-item" href="'+item.url+'" style="background-image:url(' + item.img +
-                              ');left:'+ (leftposition*i + gapX/2) +'px;top:'+(topposition + (options.height/2 +options.gap )*j)+
-                           'px" ><div class="pc-item-mask"><span class="pc-item-title" >'+item.title+'</span><span class="pc-item-subtitle">'+item.subtitle+'</span></div></a>');
+                              ');width:'+ width +'px;left:'+ (leftposition*i + options.gap/2) +'px;top:'+(topposition + (options.itemheight +options.gap )*j)+
+                           'px" data-order="'+ i +'"><div class="pc-item-mask"><span class="pc-item-title" >'+item.title+'</span><span class="pc-item-subtitle">'+item.subtitle+'</span></div></a>');
         };
 
       };
@@ -120,25 +125,25 @@
 
 
 
-      $('.pc-item').each( function (index) {
-          animate(this);
-      });
+
+          animate();
 
     };
 
     var style = function () {
       $('.pc-item').css({
-        'transition'      : 'opacity 0.3s linear',
-        'opacity'         : options.opacity,
-        'overflow'        : 'hidden',
-        'position'        : 'absolute',
-        'background-size' : 'cover',
-        '-webkit-filter'  : 'saturate(40%)',
-        'filter'          : 'saturate(40%)',
+        'transition'            : 'opacity 0.3s linear',
+        'opacity'               : options.opacity,
+        'overflow'              : 'hidden',
+        'position'              : 'absolute',
+				//'background-color'			: 'white',
+				'background-position'   : '50% 50%',
+        'background-size'       : 'cover',
+        '-webkit-filter'        : 'saturate(40%)',
+        'filter'                : 'saturate(40%)',
         //mozilla trick
-        'filter'          : 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale")',
-        'width'           : options.colswidth,
-        'height'          : options.height/2,
+        'filter'                : 'url("data:image/svg+xml;utf8,<svg xmlns=\'http://www.w3.org/2000/svg\'><filter id=\'grayscale\'><feColorMatrix type=\'matrix\' values=\'0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0.3333 0.3333 0.3333 0 0 0 0 0 1 0\'/></filter></svg>#grayscale")',
+        'height'                : options.itemheight,
       });
 
       $('.pc-item-title').css({
@@ -146,7 +151,7 @@
         'font-weight'         : 'bold',
         'transition'          : 'bottom 0.3s ease-in-out',
         'position'            : 'absolute',
-        'top'              : 10,
+        'top'              	 : 10,
         'left'                : 10,
         'color'               : '#fff'
       });
@@ -169,7 +174,8 @@
         'bottom'              : '-100px',
         'left'                : 0,
         'right'               : 0,
-        'height'              : 60,
+				'height'              : 60,
+				'width'               : '100%',
         'background-color'    : 'rgba(0,0,0,0.6)'
       });
 
